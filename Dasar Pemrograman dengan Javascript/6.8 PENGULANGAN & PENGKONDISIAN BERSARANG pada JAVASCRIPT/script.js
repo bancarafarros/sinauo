6052bf8f7bd0543eaed5bc60cf01c70ea6f02027
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-06-08 14:50:54 +0700

    gabung series

---
+++ Dasar Pemrograman dengan Javascript/6.8 PENGULANGAN & PENGKONDISIAN BERSARANG pada JAVASCRIPT/script.js
@@ -0,0 +1,19 @@
+// console.log("*****");
+
+// for (var i = 0; i < 5; i++) {
+//   console.log("*");
+// }
+
+var c = "";
+for (var i = 0; i < 5; i++) {
+  c += "*";
+  c += "\n";
+}
+console.log(c);
+
+var c = "";
+for (var i = 0; i < 5; i++) {
+  c += "*";
+  c += "\n";
+}
+console.log(c);
