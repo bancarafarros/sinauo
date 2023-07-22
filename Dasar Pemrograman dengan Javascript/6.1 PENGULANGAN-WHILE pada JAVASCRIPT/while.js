3b20fa338b51c8f5770b06c2f8d37865502338e1
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-07-22 16:43:01 +0700

    finish 6.1 PENGULANGAN-WHILE pada JAVASCRIPT

---
+++ Dasar Pemrograman dengan Javascript/6.1 PENGULANGAN-WHILE pada JAVASCRIPT/while.js
@@ -0,0 +1,24 @@
+// perulangan dihentikan oleh user
+// var ulang = true;
+
+// while (ulang) {
+//     console.log('Hello World');
+//     ulang = confirm('lagi?');
+// }
+
+// perulangan dihentikan oleh program
+// var nilaiAwal = 1
+
+// while (nilaiAwal <= 5) {
+//     console.log('Hello World');
+
+//     nilaiAwal++;
+// }
+
+var nilaiAwal = 1
+
+while (nilaiAwal <= 5) {
+    console.log('Hello World ' + nilaiAwal + 'x');
+
+    nilaiAwal++;
+}
