9a4f3203d020dcabd7197319382cc34a6c1b62cf
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-08-05 21:38:42 +0700

    finish 6.8

--- Dasar Pemrograman dengan Javascript/6.8 PENGULANGAN & PENGKONDISIAN BERSARANG pada JAVASCRIPT/script.js
+++ Dasar Pemrograman dengan Javascript/6.8 PENGULANGAN & PENGKONDISIAN BERSARANG pada JAVASCRIPT/script.js
@@ -1,19 +1,45 @@
 // console.log("*****");
 
 // for (var i = 0; i < 5; i++) {
-//   console.log("*");
+//     console.log("*");
 // }
 
-var c = "";
-for (var i = 0; i < 5; i++) {
-  c += "*";
-  c += "\n";
-}
-console.log(c);
+// var s = "";
+// for (var i = 0; i < 10; i++) {
+//     s += "*";
+// }
+// console.log(s);
 
-var c = "";
-for (var i = 0; i < 5; i++) {
-  c += "*";
-  c += "\n";
+// var s = "";
+// for (var i = 0; i < 10; i++) {
+//     s += "*";
+//     s += "\n";
+// }
+// console.log(s);
+
+// var s = "";
+// for (var i = 0; i < 7; i++) {
+//     for (var j = 0; j < 10; j++) {
+//         s += "*";
+//     }
+//     s += "\n";
+// }
+// console.log(s);
+
+// var s = "";
+// for (var i = 0; i < 15; i++) { // baris
+//     for (var j = 0; j <= i; j++) { // bintang
+//         s += "*";
+//     }
+//     s += "\n";
+// }
+// console.log(s);
+
+var s = "";
+for (var i = 10; i > 0; i--) { // baris
+    for (var j = 0; j < i; j++) { // bintang
+        s += "*";
+    }
+    s += "\n";
 }
-console.log(c);
+console.log(s);
