2473165cbcdc3f37435adc7f4f77893d15fad1d8
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-08-08 21:39:03 +0700

    finish 7.1

---
+++ Dasar Pemrograman dengan Javascript/7.1 FUNCTION pada JAVASCRIPT - Membuat dan Menjalankan FUNCTION/script.js
@@ -0,0 +1,47 @@
+// var sisiA = 8;
+// var sisiB = 3;
+
+// var volumeKubusA;
+// var volumeKubusB;
+// var jumlahVolumeKubus;
+
+// volumeKubusA = sisiA * sisiA * sisiA
+// volumeKubusB = sisiB * sisiB * sisiB
+
+// jumlahVolumeKubus = volumeKubusA + volumeKubusB;
+
+// console.log(jumlahVolumeKubus);
+
+// kj
+function jumlahVolumeDuaKubus(a, b) {
+    var volumeA;
+    var volumeB;
+    var total;
+
+    volumeA = a * a * a;
+    volumeB = b * b * b;
+
+    total = volumeA + volumeB;
+
+    return total;
+}
+
+console.log(jumlahVolumeDuaKubus(8, 3));
+
+// function hitungVolumeKubusA(sisiA) {
+//     var volumeKubusA = sisiA * sisiA * sisiA
+    
+//     return volumeKubusA;
+// }
+
+// function hitungVolumeKubusB(sisiB) {
+//     var volumeKubusB = sisiB * sisiB * sisiB
+    
+//     return volumeKubusB;
+// }
+
+// function hitungJumlahVolumeKubus(volumeKubusA, volumeKubusB) {
+//     var jumlahVolumeKubus = volumeKubusA + volumeKubusB;
+
+//     return jumlahVolumeKubus;
+// }
