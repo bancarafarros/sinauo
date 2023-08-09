ad5d1285e26bde3c2021a7d0b7dca873d7f072fa
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-08-09 14:34:06 +0700

    finish 7.3

---
+++ Dasar Pemrograman dengan Javascript/7.3 FUNCTION pada JAVASCRIPT - REFACTORING/script.js
@@ -0,0 +1,22 @@
+// sebelum refactoring
+// function jumlahVolumeDuaKubus(a, b) {
+//     var volumeA;
+//     var volumeB;
+//     var total;
+
+//     volumeA = a * a * a;
+//     volumeB = b * b * b;
+
+//     total = volumeA + volumeB;
+
+//     return total;
+// }
+
+// console.log(jumlahVolumeDuaKubus(8, 3));
+
+// sesudah refactoring
+function jumlahVolumeDuaKubus(a, b) {
+    return a * a * a + b * b * b;
+}
+
+console.log(jumlahVolumeDuaKubus(8, 3));
