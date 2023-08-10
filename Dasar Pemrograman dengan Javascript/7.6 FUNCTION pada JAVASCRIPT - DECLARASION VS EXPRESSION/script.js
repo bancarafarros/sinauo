a888a9c1bd9dad9b896734c91f1525e44cea39c8
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-08-10 22:04:57 +0700

    finish 7.6

---
+++ Dasar Pemrograman dengan Javascript/7.6 FUNCTION pada JAVASCRIPT - DECLARASION VS EXPRESSION/script.js
@@ -0,0 +1,11 @@
+function uwiww(params) {} // function declaration
+
+// contoh function declaration
+function tampilPesan(nama) {
+    alert('halo ' + nama);
+}
+
+function nameOptional(paramsOptionalToo) {} // function expression
+
+// contoh function expression
+var tampilPesan = function(nama) {alert('halo ' + nama);}
