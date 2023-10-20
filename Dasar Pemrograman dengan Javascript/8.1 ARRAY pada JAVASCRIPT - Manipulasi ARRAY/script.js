69cb3388d725e594a8707cd2d50c29ae8dc4db34
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-10-20 14:25:50 +0700

    finish 8.1 manipulasi array

---
+++ Dasar Pemrograman dengan Javascript/8.1 ARRAY pada JAVASCRIPT - Manipulasi ARRAY/script.js
@@ -0,0 +1,58 @@
+// manipulasi array
+
+// 1 menambah isi array
+// var arr = ["a", 1, true];
+// console.log(arr);
+
+// var arr = ["a", 1, true];
+// console.log(arr[1]);
+
+// var arr = [];
+// arr[0] = "satu";
+// arr[1] = "dua";
+// arr[2] = "tiga";
+// arr[6] = "asdfg";
+
+// console.log(arr);
+
+// 2 menghapus isi array
+// var arr = ["satu", "dua", "tiga", "empat", "lima"];
+// arr[1] = undefined;
+
+// console.log(arr);
+
+// 3 menampilkan isi array
+// var arr = ["sandhika", "galih", "nofa"];
+
+// for (var i = 0; i < 3; i++) {
+//     console.log(arr[i]);
+// }
+
+// var arr = ["sandhika", "galih", "nofa", "fufu"];
+
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// method pada array
+
+var arr = ["sandhika", "galih", "nofa", "fufu"];
+
+// 1 join menggabungkan semua isi array menjadi string
+// console.log(arr.join(' - '));
+
+// 2 push menambah elemen di belakang array
+// arr.push("xioa")
+// console.log(arr);
+
+// 3 pop menghapus elemen terakhir pada array
+// arr.pop();
+// console.log(arr);
+
+// 4 unshift menambah elemen di depan array
+// arr.unshift("xioa")
+// console.log(arr);
+
+// 5 shift mengahapus elemen pertama pada array
+arr.shift();
+console.log(arr);
