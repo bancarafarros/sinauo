1e62d20f48573ebb48e3db21d526ab4335bf0aec
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-10-23 14:18:18 +0700

    finish 5 struktur kendali

---
+++ Belajar PHP untuk PEMULA/5 STRUKTUR KENDALI/latihan1.php
@@ -0,0 +1,27 @@
+<?php
+    // Pengulangan
+    // for
+    // while
+    // do while
+    // foreach, pengulangan khusu array
+
+    // for ($i=0; $i < 5; $i++) { 
+    //     echo "Hello World! <br><br>";
+    // }
+
+    // while dicek dulu bari dieksekusi
+    // misal kondisinya gk sesuai dia gk eksekusi
+    // $i = 0;
+    // while ($i < 5) {
+    //     echo "Hello World! <br><br>";
+    //     $i++;
+    // }
+
+    // do while dieksekusi sekali baru dicek
+    // misal kondisinya gk sesuai dia tetep jalanin
+    $i = 0;
+    do {
+        echo "Hello World! <br><br>";
+        $i++;
+    } while ($i < 5);
+?>
---
+++ Belajar PHP untuk PEMULA/5 STRUKTUR KENDALI/latihan3.php
@@ -0,0 +1,49 @@
+<?php
+// Pengkondisian / Percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+// $x = 10;
+// if ($x < 20) {
+//     echo "benar";
+// }
+
+// $x = 20;
+// if ($x < 20) {
+//     echo "benar";
+// }
+// echo "salah"
+
+// $x = 10;
+// if ($x < 20) {
+//     echo "benar";
+// }
+// echo "salah";
+
+// $x = 10;
+// if ($x < 20) {
+//     echo "benar";
+// } else {
+//     echo "salah";
+// }
+
+// $x = 30;
+// if ($x < 20) {
+//     echo "benar";
+// } elseif ($x == 20) {
+//     echo "bingo!";
+// } else {
+//     echo "salah";
+// }
+
+$x = 20;
+if ($x < 20) {
+    echo "benar";
+} elseif ($x == 20) {
+    echo "bingo!";
+} else {
+    echo "salah";
+}
+?>
