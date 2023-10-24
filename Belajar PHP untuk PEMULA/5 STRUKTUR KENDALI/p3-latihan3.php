2040cc932befe68488565dc2d4d017f528ad2ec6
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-10-24 21:21:46 +0700

    add penjelasan ternary sama switch

--- Belajar PHP untuk PEMULA/5 STRUKTUR KENDALI/p3-latihan3.php
+++ Belajar PHP untuk PEMULA/5 STRUKTUR KENDALI/p3-latihan3.php
@@ -46,4 +46,45 @@ if ($x < 20) {
 } else {
     echo "salah";
 }
+
+// ternary
+// yang pertama aksi dari kondisi true, yg kedua aksi dari kondisi false
+// di bawah sama kayak ini, if empty($user) = TRUE, set $status = "anonymous"
+echo $status = (empty($user)) ? "anonymous" : "logged in";
+echo("<br>");
+
+$user = "John Doe";
+// di bawah sama kayak ini, if empty($user) = FALSE, set $status = "logged in"
+echo $status = (empty($user)) ? "anonymous" : "logged in";
+
+// switch
+// if else if versi lebih simpel
+
+$favcolor = "red";
+switch ($favcolor) {
+    case "red":
+        echo "Your favorite color is red!";
+        break;
+    case "blue":
+        echo "Your favorite color is blue!";
+        break;
+    case "green":
+        echo "Your favorite color is green!";
+        break;
+    default:
+        echo "Your favorite color is neither red, blue, nor green!";
+}
+
+// versi if else if
+$favcolor = "red";
+if ($favcolor == "red") {
+    echo "Your favorite color is red!";
+} else if ($favcolor == "blue") {
+    echo "Your favorite color is blue!";
+} else if ($favcolor == "green") {
+    echo "Your favorite color is green!";
+} else {
+    echo "Your favorite color is neither red, blue, nor green!";
+}
+
 ?>
