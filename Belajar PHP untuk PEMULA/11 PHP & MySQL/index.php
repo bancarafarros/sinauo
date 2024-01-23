0039822a01c265c4bb42d1a22d0e46d22f61b68e
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-01-23 15:18:47 +0700

    finish 11 php & mysql

---
+++ Belajar PHP untuk PEMULA/11 PHP & MySQL/index.php
@@ -0,0 +1,55 @@
+<?php
+// menggunakan file functions.php
+require 'functions.php';
+
+// menggunakan function query() dari file functions.php
+// function query diisi dengan sintaks SQL
+$mahasiswa = query("SELECT * FROM mahasiswa")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+
+<body>
+    <h1>Daftar Mahasiswa</h1>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+            <th>Gambar</th>
+            <th colspan="2">Aksi</th>
+        </tr>
+
+        <?php $no = 1; ?>
+        <!-- Menggunakan foreach untuk menampilkan data -->
+        <?php foreach ($mahasiswa as $row) : ?>
+            <tr>
+                <td><?= $no; ?></td>
+                <td><?= $row["nama"]; ?></td>
+                <td><?= $row["nim"]; ?></td>
+                <td><?= $row["email"]; ?></td>
+                <td><?= $row["jurusan"]; ?></td>
+                <td><img src="img/<?= $row["gambar"]; ?>" alt="" width="100"></td>
+                <td>
+                    <a href="">Ubah</a>
+                </td>
+                <td>
+                    <a href="">Hapus</a>
+                </td>
+            </tr>
+            <?php $no++; ?>
+        <?php endforeach; ?>
+    </table>
+</body>
+
+</html>
---
+++ Belajar PHP untuk PEMULA/11 PHP & MySQL/index0.php
@@ -0,0 +1,63 @@
+<?php
+// koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+// ambil data dari tabel mahasiswa / query data mahasiswa
+$result = mysqli_query($conn, "SELECT * FROM mahasiswa");
+
+// ambil data (fetch) mahasiswa dari object result
+// mysqli_fetch_row() mengembalikan array numerik
+// mysqli_fetch_assoc() mengembalikan array asosiatif
+// mysqli_fetch_array() mengembalikan array numerik dan asosiatif
+// mysqli_fetch_object() mengembalikan object
+
+// while ($mhs = mysqli_fetch_assoc($result)) {
+//     var_dump($mhs);
+// }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+
+<body>
+    <h1>Daftar Mahasiswa</h1>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+            <th>Gambar</th>
+            <th colspan="2">Aksi</th>
+        </tr>
+
+        <?php $no = 1; ?>
+        <?php while ($row = mysqli_fetch_assoc($result)) : ?>
+            <tr>
+                <td><?= $no; ?></td>
+                <td><?= $row["nama"]; ?></td>
+                <td><?= $row["nim"]; ?></td>
+                <td><?= $row["email"]; ?></td>
+                <td><?= $row["jurusan"]; ?></td>
+                <td><img src="img/<?= $row["gambar"]; ?>" alt="" width="100"></td>
+                <td>
+                    <a href="">Ubah</a>
+                </td>
+                <td>
+                    <a href="">Hapus</a>
+                </td>
+            </tr>
+            <?php $no++; ?>
+        <?php endwhile; ?>
+    </table>
+</body>
+
+</html>
