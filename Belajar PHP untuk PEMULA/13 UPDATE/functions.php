def2869c9d7d831349a2fcacfa2f82992680e702
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-04-23 12:05:48 +0700

    finish update php

---
+++ Belajar PHP untuk PEMULA/13 UPDATE/functions.php
@@ -0,0 +1,75 @@
+<?php
+// koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+// ambil data dari tabel mahasiswa / query data mahasiswa
+function query($query)
+{
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+
+    return $rows;
+}
+
+function tambahMahasiswa($data)
+{
+    global $conn;
+    // var_dump($_POST);
+
+    // ambil data dari tiap elemen dalam form
+    // htmlspecialchars anti script
+    $nim = htmlspecialchars($data["nim"]);
+    $nama = htmlspecialchars($data["nama"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+
+    // query insert data
+    // $query = "INSERT INTO mahasiswa values ('', '$nim', '$nama', '$email', '$jurusan', '$gambar')";
+    $query = "INSERT INTO mahasiswa (nim, nama, email, jurusan, gambar) VALUES ('$nim', '$nama', '$email', '$jurusan', '$gambar')";
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+function hapusMahasiswa($id)
+{
+    global $conn;
+
+    mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id");
+
+    return mysqli_affected_rows($conn);
+}
+
+function updateMahasiswa($data)
+{
+    global $conn;
+
+    // ambil data dari tiap elemen dalam form
+    // htmlspecialchars anti script
+    $id = htmlspecialchars($data["id"]);
+    $nim = htmlspecialchars($data["nim"]);
+    $nama = htmlspecialchars($data["nama"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+
+    // query update data
+    $query = "UPDATE mahasiswa SET
+    nim = '$nim',
+    nama = '$nama',
+    email = '$email',
+    jurusan = '$jurusan',
+    gambar = '$gambar'
+    WHERE id = $id
+    ";
+
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
