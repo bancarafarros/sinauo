732f02d1ed24aabc06e1ce3d89bf975972218d2b
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-05-30 08:31:22 +0700

    start 17-LOGIN

---
+++ wpu-Belajar PHP untuk PEMULA/17-LOGIN/index.php
@@ -0,0 +1,69 @@
+<?php
+// menggunakan file functions.php
+require 'functions.php';
+
+// menggunakan function query() dari file functions.php
+// function query diisi dengan sintaks SQL
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+if (isset($_POST["cari"])) {
+    $mahasiswa = cari($_POST["keyword"]);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+
+<body>
+    <h1>Daftar Mahasiswa</h1>
+
+    <a href="tambah.php">Tambah Data Mahasiswa</a>
+    <br><br>
+
+    <form action="" method="post">
+        <!-- autofocus = ngarahin kursor  autocomplete="off" = matiin suggestion-->
+        <input type="text" name="keyword" size="40" placeholder="Masukkan keyword pencarian" autofocus autocomplete="off">
+        <button type="submit" name="cari">Cari</button>
+    </form>
+    <br><br>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+            <th>Gambar</th>
+            <th colspan="2">Aksi</th>
+        </tr>
+
+        <?php $no = 1; ?>
+        <!-- Menggunakan foreach untuk menampilkan data -->
+        <?php foreach ($mahasiswa as $row) : ?>
+            <tr>
+                <td><?= $no; ?></td>
+                <td><?= $row["nama"]; ?></td>
+                <td><?= $row["nim"]; ?></td>
+                <td><?= $row["email"]; ?></td>
+                <td><?= $row["jurusan"]; ?></td>
+                <td><img src="img/<?= $row["gambar"]; ?>" alt="" width="100"></td>
+                <td>
+                    <a href="update.php?id=<?= $row["id"]; ?>">Ubah</a>
+                </td>
+                <td>
+                    <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('Yakin ingin hapus data?');">Hapus</a>
+                </td>
+            </tr>
+            <?php $no++; ?>
+        <?php endforeach; ?>
+    </table>
+</body>
+
+</html>
---
+++ wpu-Belajar PHP untuk PEMULA/17-LOGIN/registrasi.php
@@ -0,0 +1,56 @@
+<?php
+require 'functions.php';
+
+// ketika tombol register submit ditekan maka data user baru akan divalidasi dan diinput ke database
+if (isset($_POST["register"])) {
+
+    if (registrasi($_POST) > 0) { // kondisi ketika berhasil membuat user baru
+        echo "<script>alert('User baru berhasil ditambahkan')</script>";
+    } else { // kondisi ketika gagal membuat user baru
+        echo mysqli_error($conn);
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Registrasi</title>
+    <style>
+        label {
+            display: block;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Halaman Registrasi</h1>
+
+    <!-- attribue action dikosongkan supaya data yang disubmit dikembalikan ke halaman tempat dia berasal -->
+    <!-- jika  diisi, maka data akan dikrimkan ke halaman yang dituliskan di dalam attribute action-->
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="username">Username:</label>
+                <input type="text" name="username" id="username">
+            </li><br>
+
+            <li>
+                <label for="password">Password:</label>
+                <input type="password" name="password" id="password">
+            </li><br>
+
+            <li>
+                <label for="confirmPassword">Konfirmasi Password:</label>
+                <input type="password" name="confirmPassword" id="confirmPassword">
+            </li><br>
+
+            <button type="submit" name="register">Register</button>
+        </ul>
+    </form>
+</body>
+
+</html>
