9424199855f5c1db2a24a1664b74e36a122c2f0a
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-05-31 11:11:53 +0700

    start 19-COOKIE

---
+++ wpu-Belajar PHP untuk PEMULA/19-COOKIE/login.php
@@ -0,0 +1,79 @@
+<?php
+// session adalah informasi yang disimpan di sisi server
+session_start();
+
+// validasi user sudah atau belum untuk mencegah function digunakan non user
+if (isset($_SESSION["login"])) {
+    header("Location: index.php"); // jika sudah login diarahkan ke index.php
+    exit;
+}
+
+// import functions.php
+require 'functions.php';
+
+// ambil data dari form login via $_POST
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // cek username dulu, kalo ada lanjut cek password kalo gk ada skip
+
+    // cek table users di database, apakah ada username yang sama
+    $result = mysqli_query($conn, "SELECT * FROM users WHERE username = '$username'");
+
+    // kalo ada username yang sama, pasti hasilnya 1
+    if (mysqli_num_rows($result) === 1) {
+        // cek password dari username yang sama
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($password, $row['password'])) {
+
+            // set session
+            $_SESSION["login"] = true;
+
+            header("Location: index.php");
+            exit;
+        }
+    }
+
+    // kalo username gk ketemu masuk ke sini, $error buat display error message di login page
+    $error = true;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Login</title>
+</head>
+
+<body>
+    <h1>Halaman Login</h1>
+
+    <!-- error message -->
+    <?php if (isset($error)) : ?>
+        <p style="color: red; font-style:italic">Username atau password salah</p>
+    <?php endif; ?>
+
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="username">Username: </label>
+                <input type="text" name="username" id="username">
+            </li><br>
+
+            <li>
+                <label for="password">Password: </label>
+                <input type="password" name="password" id="password">
+            </li><br>
+
+            <li>
+                <button type="submit" name="login">Login</button>
+            </li>
+        </ul>
+    </form>
+</body>
+
+</html>
---
+++ wpu-Belajar PHP untuk PEMULA/19-COOKIE/tambah.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+
+// validasi user sudah atau belum untuk mencegah function digunakan non user
+if (!isset($_SESSION["login"])) {
+    header("Location: login.php"); // jika belum login diarahkan ke login.php
+    exit;
+}
+
+// menggunakan file functions.php
+require 'functions.php';
+
+// cek apakah tombol submit sudah pernah ditekan atau belum
+if (isset($_POST["submit"])) {
+    // var_dump($_POST);
+    // var_dump($_FILES);
+    // die;
+
+    // cek data masuk atau tidak ke db
+    if (tambahMahasiswa($_POST) > 0) {
+        echo "
+        <script>
+            alert('Data berhasil ditambahkan');
+            document.location.href = 'index.php'
+        </script>";
+    } else {
+        echo "
+        <script>
+            alert('Data gagal ditambahkan');
+            document.location.href = 'index.php'
+        </script>";
+    }
+
+    // // cek data masuk atau tidak ke db
+    // if (var_dump(mysqli_affected_rows($conn) > 0)) {
+    //     echo "berhasil";
+    // } else {
+    //     echo "gagal";
+    //     echo "<br>";
+    //     echo mysqli_error($conn);
+    // }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data Mahasiswa</title>
+</head>
+
+<body>
+    <h1>Tambah Data Mahasiswa</h1>
+
+    <form action="" method="post" enctype="multipart/form-data">
+        <ul>
+            <li>
+                <label for="nim">NIM</label>
+                <input type="text" name="nim" id="nim" required>
+            </li>
+
+            <li>
+                <label for="nama">Nama</label>
+                <input type="text" name="nama" id="nama" required>
+            </li>
+
+            <li>
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email" required>
+            </li>
+
+            <li>
+                <label for="jurusan">Jurusan</label>
+                <input type="text" name="jurusan" id="jurusan" required>
+            </li>
+
+            <li>
+                <label for="gambar">Gambar</label>
+                <input type="file" name="gambar" id="gambar" required>
+            </li>
+
+            <li>
+                <button type="submit" name="submit">Submit</button>
+            </li>
+        </ul>
+    </form>
+</body>
+
+</html>
