a0d735cbdb514e54edd6e46f24f7a3a33830dec2
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-06-11 22:41:44 +0700

    rename + add notes 1 Pendahuluan

---
+++ wpu-OOP DASAR pada PHP/1 Pendahuluan/.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>procedural programming</h1>
+    <p>
+        > instruksi dilakukan langkah demi langkah
+        > memecah program menjadi bagian-bagian (function/prosedur) yang lebih kecil
+        > bagian yang lebih kecil merupakan function/method/procedure/subroutine
+        > dibaca secara linear/top to bottom
+    </p>
+
+    <h1>
+        kelebihan procedural programming
+    </h1>
+    <p>
+        > to the point, karena langsung menulis instruksi
+        > simplicity dan kemudahan implementasi untuk compiler dan interpreter, karena to the point
+        > mudah ditelusuri, karena linear/top to bottom
+        > membutuhkan lebih sedikit memory, karena to the point
+    </p>
+
+    <h1>
+        OOP
+    </h1>
+    <p>
+        > menyusun semua kode program dan strukdat sebagai objek
+        > objek adalah unit dasar dari program
+        > bisa menyimpan data dan perilaku di dalam objek
+        > objek bisa saling berinteraksi
+    </p>
+
+    <h1>
+        kelebihan OOP
+    </h1>
+    <p>
+        > lebih mudah merepesentasi dunia nyata
+        > enkapsulasi untuk mencegah bagian program tertentu dijalankan di luar lingkupnya
+        > abstraksi data untuk menyembunyikan kerumitan program dengan objek
+        > reusability untuk menggunakan bagian program secara berulang kali
+        > memudahkan skalibilitas & ekstensibilitas
+        > kemudahan pengelolaan program
+        > kemudahan ketika kolaborasi
+        > digunakan oleh framework
+    </p>
+</body>
+
+</html>
