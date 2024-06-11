9ac4fab715897b3d3312e940a7656a85ff7ec19c
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-06-11 21:16:09 +0700

    add notes SQL - 02 Pengurutan Data dengan ORDER BY

--- inabel-BelajarDatabaseMySQLMariaDB/SQL 02 - Pengurutan Data dengan ORDER BY.sql
+++ inabel-BelajarDatabaseMySQLMariaDB/SQL 02 - Pengurutan Data dengan ORDER BY.sql
@@ -1,59 +1,60 @@
-show databases;
-use classicmodels;
-show tables;
-describe employees;
-select * from employees;
+show databases; -- menampilkan seluruh database yang tersedia
+use classicmodels; -- menggunakan database classicmodels
+show tables; -- menampilkan seluruh table yang tersedia di dalam database
+describe employees; -- menampilkan detail table employees
+select * -- memilih semua kolom
+from employees; -- di table employees
 
 -- menampilkan data secara urut berdasarkan kolom lastName
 -- order by secara default mengurutkan data secara ascending  
-select *
-from employees
-order by lastName;
+select * -- memilih semua kolom
+from employees -- di table employees
+order by lastName; -- mengurutkan data berdasarkan kolom lastName secar aascending (default)
 
 -- menampilkan data secara urut berdasarkan kolom lastName secara descending
-select *
-from employees
-order by lastName desc;
+select * -- memilih semua kolom
+from employees -- di table employees
+order by lastName desc; -- mengurutkan data berdasarkan kolom lastName secara descending
 
 -- menampilkan data secara urut berdasarkan lastName dan firstName tiap baris
-select *
-from employees
-order by lastName, firstName;
+select * -- memilih semua kolom
+from employees -- di table employees
+order by lastName, firstName; -- mengurutkan data berdasarkan kolom lastName dan firstName secara ascending (default) untuk keduanya 
 
 -- menampilkan data secara urut berdasarkan lastName secara ascending dan fisrtName secara descending
-select *
-from employees
-order by lastName, firstName desc;
+select * -- memilih semua kolom
+from employees -- di table employees
+order by lastName, firstName desc; -- mengurutkan data berdasarkan kolom lastName secara ascending (default) dan firstName secara descending
 
 -- menampilkan data lastName, firstName, dan jobTitle dengan diurutkan berdasarkan lastName
-select lastName, firstName, jobTitle
-from employees
-order by lastName;
+select lastName, firstName, jobTitle -- memilih kolom lastName, firstName, dan jobTitle
+from employees -- di table employees
+order by lastName; -- mengurutkan data berdasarkan kolom lastName secara ascending (default)
 
-describe orderdetails;
+describe orderdetails; -- menampilkan detail dari table orderdetails
 
 -- membuat kolom baru dengan cara quantityOrdered * priceEach dari table orderdetails dan diurutkan berdasarkan hasil quantityOrdered * priceEach secara ascending
-select orderNumber, quantityOrdered * priceEach
-from orderdetails
-order by quantityOrdered * priceEach;
+select orderNumber, quantityOrdered * priceEach -- memilih kolom orderNumber dan quantityOrdered * priceEach
+from orderdetails -- di table orderdetails
+order by quantityOrdered * priceEach; -- mengurutkan data berdasarkan kolom quantityOrdered * priceEach secara ascending (default)
 
 -- menampilkan data dari kolom orderNumber dan quantityOrdered * priceEach dari table orderdetails dan diurutkan berdasarkan hasil quantityOrdered * priceEach secara descending
-select orderNumber, quantityOrdered * priceEach
-from orderdetails
-order by quantityOrdered * priceEach desc;
+select orderNumber, quantityOrdered * priceEach -- memilih kolom orderNumber dan quantityOrdered * priceEach
+from orderdetails -- di table orderdetails
+order by quantityOrdered * priceEach desc; -- mengurutkan data berdasarkan quantityOrdered * proceEach secara descending
 
 -- menyederhanakan kolom baru hasil quantityOrdered * priceEach dengan alias atau as menjadi subtotal
-select orderNumber, quantityOrdered * priceEach as subtotal
-from orderdetails
-order by subtotal desc;
+select orderNumber, quantityOrdered * priceEach as subtotal -- memilih kolom orderNumber dan quantityOrdered * priceEach yang disederhanakan menjadi subtotal dengan as alias
+from orderdetails -- di table orderdetails
+order by subtotal desc; -- mengurutkan data berdasarkan kolom baru subtotal secara descending
 
 -- tugas
 -- mencari harga satuan terbesar dari table orderdetails
-select *
-from orderdetails
-order by priceEach desc;
+select * -- memilih semua kolom
+from orderdetails -- di table orderdetails
+order by priceEach desc; -- mengurutkan data berdasarkan kolom priceEach secara descending
 
 -- menampilkan data customer yang terurut secara alfabet berdasarkan nama belakang
-select *
-from customers
-order by contactLastName;
+select * -- memilih semua kolom
+from customers -- di table customers
+order by contactLastName; -- mengurutkan data berdasarkan kolom contactLastName secara ascending (default)
