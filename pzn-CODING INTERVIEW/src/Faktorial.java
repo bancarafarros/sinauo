a62a7d2ebad56f244ce4a7f0dc3bd5510ee1ec01
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-06-13 23:20:57 +0700

    rename directory

---
+++ pzn-CODING INTERVIEW/src/Faktorial.java
@@ -0,0 +1,5 @@
+public class Faktorial {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ pzn-CODING INTERVIEW/src/StrukdatSet.java
@@ -0,0 +1,5 @@
+public class StrukdatSet {
+    public static void main(String[] args) {
+
+    }
+}
