22d3c296dae01dbe0c3d30aa0c4322775c2262f4
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-06-24 22:58:38 +0700

    finish 6 Inheritance (1)

--- wpu-OOP DASAR pada PHP/6 Inheritance (1)/inheritance-problem.php
+++ wpu-OOP DASAR pada PHP/6 Inheritance (1)/inheritance-problem.php
@@ -7,20 +7,25 @@ class Product
         $judul,
         $penulis,
         $penerbit,
-        $harga;
+        $harga,
+        $jumlahHalaman,
+        $waktuMain,
+        $tipe;
 
     // contructor adalah function/method yang otomatis dijalankan ketika sebuah class memiliki object baru
     // constructor biasa digunakan sebagai nilai default
 
-
     // parameter di __construct berbeda dengan property class dan sudah diberi nilai default
-    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jumlahHalaman = 0, $waktuMain = 0, $tipe = "Tipe")
     {
         // mengisi property class dengan value dari parameter __construct
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
+        $this->jumlahHalaman = $jumlahHalaman;
+        $this->waktuMain = $waktuMain;
+        $this->tipe = $tipe;
     }
 
     // method
@@ -33,6 +38,25 @@ class Product
     {
         // pake this biar bisa make property di luar function
         return "$this->penulis, $this->penerbit";
+    }
+
+    // getInfoLengkap untuk menmapilkan informasi detail dari produk dengan menerapkan inheritance
+    public function getInfoLengkap()
+    {
+        // expected result
+        // Komik: Naruto | Masashi Kishimoto, Shonen Jump (Rp 30000) - 100 halaman
+        // Komik: Uncharted | Neil Druckmann, Sony Computer (Rp 25000) - 50 jam
+
+        // {} untuk sebagai delimiter karena ditulis dalam bentuk string ""
+        $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp {$this->harga})"; // mengambil value dari property class yang berasal dari contructor
+
+        if ($this->tipe == "Komik") { // jika tipenya komik
+            $str .=  " - {$this->jumlahHalaman} halaman"; // maka yang ditampilkan adalah jumlah halaman
+        } else if ($this->tipe == "Game") { // jika tipenya game
+            $str .=  " - {$this->waktuMain} jam"; // maka yang ditampilkan adalah waktu main
+        }
+
+        return $str; // mengambalikan $str supaya bisa ditampilkan di layar
     }
 }
 
@@ -51,16 +75,22 @@ class CetakInfoProduct // class yang akan menerima inputan object
 }
 
 // membuat object product dari class Product dan mengisi parameter untuk __construct
-$productSatu = new Product("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
-$productDua = new Product("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
+$productSatu = new Product("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0, "Komik");
+$productDua = new Product("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50, "Game");
 $productTiga = new Product("Dragon Ball"); // hanya mengisi satu parameter __construct karena sisanya sudah ada nilai default
 
-echo "Komik: " . $productSatu->getLabel();
-print_r("<br><br>");
-echo "Game: " . $productDua->getLabel();
-print_r("<br><br>");
-echo "Game: " . $productTiga->getLabel();
-print_r("<br><br>");
+// echo "Komik: " . $productSatu->getLabel();
+// print_r("<br><br>");
+// echo "Game: " . $productDua->getLabel();
+// print_r("<br><br>");
+// echo "Game: " . $productTiga->getLabel();
+// print_r("<br><br>");
 
-$infoProductSatu = new CetakInfoProduct(); // instansiasi object $infoProductSatu dari class CetakInfoProduct
-echo $infoProductSatu->cetak($productSatu); // menampilkan detail dari $productSatu via object $infoProductSatu dan class CetakInfoProduct
+// $infoProductSatu = new CetakInfoProduct(); // instansiasi object $infoProductSatu dari class CetakInfoProduct
+// echo $infoProductSatu->cetak($productSatu); // menampilkan detail dari $productSatu via object $infoProductSatu dan class CetakInfoProduct
+
+// menjalankan getInfoLengkap() yang menerapkan inheritance
+echo $productSatu->getInfoLengkap();
+print_r("<br><br>");
+echo $productDua->getInfoLengkap();
+print_r("<br><br>");
