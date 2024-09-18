22f6cacc832f9d4d83d3a537c6962b8ca1870f63
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-09-18 11:02:53 +0700

    pzn-JAVA-DASAR: finish Tipe Data Boolean

---
+++ pzn-JAVA-DASAR/src/TipeDataBoolean.java
@@ -0,0 +1,12 @@
+public class TipeDataBoolean {
+    public static void main(String[] args) {
+        // cuma punya 2 nilai, true dan false
+        // defaultnya false
+
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println(benar);
+        System.out.println(salah);
+    }
+}
