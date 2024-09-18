f6e633cade5e0205d18508724641a1c19b6caf4e
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-09-18 11:03:01 +0700

    pzn-JAVA-DASAR: finish Tipe Data String

---
+++ pzn-JAVA-DASAR/src/TipeDataString.java
@@ -0,0 +1,16 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        // isinya kumpulan character alias teks
+        // di java, string pake ""
+        // default value string di java adalah null
+
+        String firstName = "Charr";
+        String lastName = "Gonn";
+
+        String fullName = firstName + " " + lastName; // concatenation string alias penggabungan 2 string atau lebih
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(fullName);
+    }
+}
