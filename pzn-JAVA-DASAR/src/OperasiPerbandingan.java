cc9621526a012f2bf084e5e09a310180770ffb41
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-09-24 11:38:46 +0700

    pzn-JAVA-DASAR: finish Operasi Perbandingan

---
+++ pzn-JAVA-DASAR/src/OperasiPerbandingan.java
@@ -0,0 +1,17 @@
+public class OperasiPerbandingan {
+    public static void main(String[] args) {
+        // untuk membandingan 2 buat data atau lebih
+        // menghasilkan nilai boolean, benar atau salah alias true or false
+        // >, <, >=, <=, ==, !=
+
+        int a = 100;
+        int b = 100;
+
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+}
